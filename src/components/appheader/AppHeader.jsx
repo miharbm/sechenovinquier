45cbc6b3c6b7f5e496455ddc6c487926042c294c
miharbm <m.borodin@inlinegroup.ru>
2025-03-18 19:22:05 +0300

    migrate to tabs in app header

--- src/components/appheader/AppHeader.jsx
+++ src/components/appheader/AppHeader.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
@@ -13,72 +13,54 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Divider from '@mui/material/Divider';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import { useAuth } from '../../context/AuthContext.jsx';
 import { useGetUserInfoQuery } from '../../api/api.js';
 import Grid from "@mui/material/Grid";
-// import {ReactComponent as  IconLogo} from "../../assets/icon.svg"
-import IconLogo from "../../assets/icon.svg"
 import Logo from "../logo/Logo.jsx";
-
+import {ListItemButton} from "@mui/material";
 
 const AppHeader = ({ doctorId, doctorUsername }) => {
     const { logout } = useAuth();
     const { data: userData } = useGetUserInfoQuery({ userId: doctorId });
     const [menuOpen, setMenuOpen] = useState(false);
+    const location = useLocation();
 
     const links = [
-        {
-            title: "Главная",
-            link: "/",
-        },
-        {
-            title: "Регистрация пациента",
-            link: "/patient-registration",
-        },
-        {
-            title: "Пациенты",
-            link: "/patients",
-        },
-    ]
+        { title: "Главная", link: "/" },
+        { title: "Регистрация пациента", link: "/patient-registration" },
+        { title: "Пациенты", link: "/patients" },
+    ];
 
-    const handleClickLogout = () => {
-        logout();
-    };
-
-    const openDrawer = () => setMenuOpen(true)
-    const closeDrawer = () => setMenuOpen(false)
-
-    const handleLinkClick = () => {
-        closeDrawer();
-    }
+    const handleClickLogout = () => logout();
+    const openDrawer = () => setMenuOpen(true);
+    const closeDrawer = () => setMenuOpen(false);
+    const handleLinkClick = () => closeDrawer();
 
     return (
         <AppBar position="static">
             <Toolbar>
-                <Box sx={{flexGrow: 1}}>
-                    <Logo/>
+                <Box sx={{ flexGrow: 1 }}>
+                    <Logo />
                 </Box>
-                <Box sx={{ display: { xs: 'none', lg: 'flex' }, alignItems: 'center', flexGrow: 2 }}>
-                    <Grid container spacing={3}>
-                        {
-                            links.map(({link, title}, index) => (
-                                <Grid item key={`link-${index}`}>
-                                    <Typography variant="h6">
-                                        <Link to={link}>{title}</Link>
-                                    </Typography>
-                                </Grid>
-                            ))
-                        }
-                    </Grid>
-                    <Typography variant="h6" sx={{ marginRight: 2 }}>
-                        {userData?.first_name ? `${userData.first_name} ${userData.last_name}` : doctorUsername}
-                    </Typography>
-                    <IconButton color="inherit">
-                        <AccountCircleIcon />
-                    </IconButton>
-                    <IconButton color="inherit" onClick={handleClickLogout}>
-                        <ExitToAppIcon />
-                    </IconButton>
+                <Box sx={{ display: { xs: 'none', lg: 'flex' }, alignItems: 'center', flexGrow: 2, justifyContent: "space-between" }}>
+                    <Tabs value={location.pathname} textColor="inherit" indicatorColor="secondary">
+                        {links.map(({ link, title }) => (
+                            <Tab key={link} label={title} component={Link} to={link} value={link} />
+                        ))}
+                    </Tabs>
+                    <Box sx={{display: "flex", alignItems: "center"}}>
+                        <Typography variant="h6" sx={{ marginRight: 2 }}>
+                            {userData?.first_name ? `${userData.first_name} ${userData.last_name}` : doctorUsername}
+                        </Typography>
+                        <IconButton color="inherit">
+                            <AccountCircleIcon />
+                        </IconButton>
+                        <IconButton color="inherit" onClick={handleClickLogout}>
+                            <ExitToAppIcon />
+                        </IconButton>
+                    </Box>
                 </Box>
                 <Box sx={{ display: { xs: 'block', lg: 'none' } }}>
                     <IconButton color="inherit" onClick={openDrawer}>
@@ -89,18 +71,11 @@ const AppHeader = ({ doctorId, doctorUsername }) => {
 
             <Drawer anchor="right" open={menuOpen} onClose={closeDrawer}>
                 <List sx={{ width: 250 }}>
-                    {
-                        links.map(({link, title}, index) => (
-                            <ListItem component={Link}
-                                      to={link}
-                                      onClick={handleLinkClick}
-                                      key={`link_${index}`}
-                                      button
-                            >
-                                <ListItemText primary={title} />
-                            </ListItem>
-                        ))
-                    }
+                    {links.map(({ link, title }) => (
+                        <ListItemButton key={link} to={link} onClick={handleLinkClick} selected={location.pathname === link}>
+                            <ListItemText primary={title} />
+                        </ListItemButton>
+                    ))}
                     <Divider />
                     <ListItem>
                         <ListItemText primary={userData?.first_name ? `${userData.first_name} ${userData.last_name}` : doctorUsername} />
@@ -109,7 +84,7 @@ const AppHeader = ({ doctorId, doctorUsername }) => {
                         <AccountCircleIcon sx={{ marginRight: 1 }} />
                         <ListItemText primary="Профиль" />
                     </ListItem>
-                    <ListItem button onClick={handleClickLogout} >
+                    <ListItem button onClick={handleClickLogout}>
                         <ExitToAppIcon sx={{ marginRight: 1 }} />
                         <ListItemText primary="Выход" />
                     </ListItem>
