e6d525746c9020237fd2f564e458019de83de785
miharbm <m.borodin@inlinegroup.ru>
2025-04-21 02:31:50 +0300

    stylize loading in ProtectedRoute.jsx

--- src/protectedroute/ProtectedRoute.jsx
+++ src/protectedroute/ProtectedRoute.jsx
@@ -1,12 +1,29 @@
 import { Navigate } from 'react-router-dom';
 import {useAuth} from "../context/AuthContext.jsx";
+import {CircularProgress} from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 const ProtectedRoute = ({ children }) => {
     const { isAuthenticated, isLoading } = useAuth();
 
 
     if (isLoading) {
-        return <div>Загрузка...</div>
+        return (
+            <Box
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                height="100svh"
+                flexDirection="column"
+                gap={3}
+            >
+                <CircularProgress size={70}/>
+                <Typography variant="h5" color="textSecondary" sx={{ml: "10px"}}>
+                    Загрузка...
+                </Typography>
+            </Box>
+        )
     }
 
     if (!isAuthenticated) {
