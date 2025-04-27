606f35508f4ae72fc72fc50b949aaa4c0a9e93c3
miharbm <m.borodin@inlinegroup.ru>
2025-04-27 20:59:52 +0300

    fix breadcrumbs for doctor

--- src/components/layouts/DoctorRoleLayout.jsx
+++ src/components/layouts/DoctorRoleLayout.jsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom";
 import AppHeader from "../appheader/AppHeader";
 import { useAuth } from "../../context/AuthContext";
-import Breadcrumbs from "../Breadcrumbs/Breadcrumbs.jsx";
 import {NavigationListProvider} from "../../context/NavigationListContext.jsx";
 
 const DoctorRoleLayout = () => {
     const { username, userId } = useAuth();
 
-    // Объединённый массив для ссылок и хлебных крошек
     const navigation = [
-        { title: "Главная", link: "/", breadcrumb: "Главная" },
-        { title: "Регистрация пациента", link: "/patient-registration", breadcrumb: "Регистрация пациента" },
-        { title: "Пациенты", link: "/patients", breadcrumb: "Пациенты" },
+        { title: "Главная", link: "/", breadcrumb: "Главная", isShowLink: true },
+        { title: "Регистрация пациента", link: "/patient-registration", breadcrumb: "Регистрация пациента", isShowLink: true },
+        { title: "Пациенты", link: "/patients", breadcrumb: "Пациенты", isShowLink: true },
+        { title: "Опрос", link: "/inquirer", breadcrumb: "Опрос", isShowLink: false },
     ];
 
-    const links = navigation.map(({ title, link }) => ({ title, link }));
+    const links = navigation.map(({ title, link, isShowLink }) => (isShowLink && { title, link }));
 
     return (
         <NavigationListProvider navigation={navigation}>
